afeba48fcf0712e668ca8c7bb163268fa9683450
biraj-phuyal <birajphuyal11@gmail.com>
2025-08-13 14:44:47 +0100

    Fixing repo

---
+++ c00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: biphuyal <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/27 15:43:11 by biphuyal          #+#    #+#             */
+/*   Updated: 2025/01/27 15:44:17 by biphuyal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	alpha;
+
+	alpha = 'z';
+	while (alpha >= 'a')
+	{
+		write(1, &alpha, 1);
+		alpha--;
+	}
+}
+
+/*
+int main()
+{
+        ft_print_reverse_alphabet();
+}
+*/
---
+++ c00/ex06/ft_print_comb2.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: biphuyal <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/27 16:02:46 by biphuyal          #+#    #+#             */
+/*   Updated: 2025/01/27 17:52:28 by biphuyal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	two_digits(int a, int b)
+{
+	ft_putchar('0' + a / 10);
+	ft_putchar('0' + a % 10);
+	ft_putchar(' ');
+	ft_putchar('0' + b / 10);
+	ft_putchar('0' + b % 10);
+}
+
+void	ft_print_comb2(void)
+{
+	int	a;
+	int	b;
+
+	a = 0;
+	while (a <= 99)
+	{
+		b = a + 1;
+		while (b <= 99)
+		{
+			two_digits(a, b);
+			if (!(a == 98 && b == 99))
+			{
+				ft_putchar(',');
+				ft_putchar(' ');
+			}
+			b++;
+		}
+		a++;
+	}
+}
+
+/*
+int main()
+{
+	ft_print_comb2();
+}
+*/
